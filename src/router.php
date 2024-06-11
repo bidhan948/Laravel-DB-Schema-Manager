975913005a75de2cb5419251bd4a470b7cefc707
uddhav.baniya <uddhav.baniya@midastechnologies.com.np>
2024-06-11 16:59:31 +0545

    Middleware added

--- src/router.php
+++ src/router.php
@@ -3,18 +3,21 @@
 use Bidhan\Bhadhan\Http\Controllers\SchemaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("bhadhan/dashboard", function () {
-    return view('Bhadhan::dashboard');
+
+Route::middleware(config('bhadhan.auth_middleware'))->group(function () {
+    Route::get("bhadhan/dashboard", function () {
+        return view('Bhadhan::dashboard');
+    });
+
+    Route::get("bhadhan/db-manager/schema", [SchemaController::class, 'index'])
+        ->name('bhadhan-db-manager.schema');
+
+    Route::get("bhadhan/db-manager/performance-metrics", [SchemaController::class, 'performanceMetrics'])
+        ->name('bhadhan-db-manager.performance');
+
+    Route::get("bhadhan/db-manager/sql", [SchemaController::class, 'sql'])
+        ->name('bhadhan-db-manager.sql');
+
+    Route::post("bhadhan/db-manager/sql", [SchemaController::class, 'sqlToData'])
+        ->name('bhadhan-db-manager.sqlToData');
 });
-
-Route::get("bhadhan/db-manager/schema", [SchemaController::class, 'index'])
-    ->name('bhadhan-db-manager.schema');
-
-Route::get("bhadhan/db-manager/performance-metrics", [SchemaController::class, 'performanceMetrics'])
-    ->name('bhadhan-db-manager.performance');
-
-Route::get("bhadhan/db-manager/sql", [SchemaController::class, 'sql'])
-    ->name('bhadhan-db-manager.sql');
-
-Route::post("bhadhan/db-manager/sql", [SchemaController::class, 'sqlToData'])
-    ->name('bhadhan-db-manager.sqlToData');
